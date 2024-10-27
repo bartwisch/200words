7e46ccbc50362a4d9bbb168687a16dd497578d5b
bartwisch <123270793+bartwisch@users.noreply.github.com>
2024-10-27 15:55:23 +0100

    v1 ohne stift

---
+++ words.js
@@ -0,0 +1,65 @@
+const wordsText = `A
+aber  alle  als  alt  am  an  andere  auch  auf  aus  Auto
+
+B
+bald  Ball  bauen  Baum  bei  bekommen  Bett  bis  bleiben  Boden  brauchen  bringen  Brot  Bruder
+
+D
+da  dann  das  den  denken  der  dich  die  doch  dort  drei  durch  dürfen
+
+E
+ein eine einer  einmal  Eltern  Ende  er  erzählen  es  essen Essen  etwas
+
+F
+fahren  fallen  fangen  fertig  finden  fliegen  fragen  Frau  Freund  für
+
+G
+ganz  geben  gehen  Geld  gern gerne  gleich  groß große  gut
+
+H
+haben  halten  Hand  Haus  heißen  heute  hier  hoch  holen  hören  Hund
+
+I
+ich  ihr ihre ihrem ihren ihrer  im  immer  in  ist
+
+J
+ja  Jahr  jetzt  Junge
+
+K
+kann  Katze  kaufen  kein  Kind  Klasse  klein  kommen  können  Kopf  kurz
+
+L
+lang lange  lassen  laufen  laut  leben Leben  legen  lesen  Leute  liegen
+
+M
+machen  Mädchen  man  Mann  mehr  mein  mit  müssen  Mutter Mutti
+
+N
+nach  nehmen  nein  neu  nicht  nichts  noch  nun  nur
+
+O
+oder
+
+P
+Pferd
+
+R
+rennen  richtig  rot  rufen
+
+S
+sagen  schlafen  schnell  schon  schön  schreiben  Schule  schwarz  Schwester  schwimmen  sehen  sehr  sich  sie  sind  sitzen  so  sollen  Sonne  spielen  sprechen  springen  Stadt  stehen  stellen  Stunde
+
+T
+Tag  Tier  Tisch  tragen  trinken  tun  Tür
+
+U
+über  Uhr  um  und  uns  unser  unter
+
+V
+Vater Vati  viel viele  vier  Vogel  voll  vom  von  vor
+
+W
+Wagen  Wald  war waren  warten  was  Wasser  Weg  weiß  weit weiter  wenn  werden  wie  wieder  wir  wird  wissen Wissen  wo  Woche  wollen
+
+Z
+Zeit  zu  zwei`;
